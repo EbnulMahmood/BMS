12bb17fabaaabfc295b8cc0af53da4a51a648faf
shovan <shovonmahmood67@gmail.com>
2023-02-28 17:37:12 +0600

    added task load usecase

---
+++ src/BMS.CoreBusiness/Dtos/DevTaskDto.cs
@@ -0,0 +1,33 @@
+﻿using BMS.CoreBusiness.Enums;
+
+namespace BMS.CoreBusiness.Dtos
+{
+    public sealed class DevTaskDto
+    {
+        public Guid Id { get; set; }
+        public string? Title { get; set; }
+        public Status Status { get; set; }
+        public Priority Priority { get; set; }
+        public string? Project { get; set; }
+        public string? UXDesignLink { get; set; }
+        public string? Group { get; set; }
+        public string? EntryBy { get; set; }
+        public string? Responsible1 { get; set; }
+        public string? Responsible2 { get; set; }
+        public int Release { get; set; }
+        public decimal? EstimatedHours { get; set; }
+        public decimal? ActualHours { get; set; }
+        public string? FRSMenuLink { get; set; }
+        public string? UrlOrMenuOrWorkflow { get; set; }
+        public string? Remarks { get; set; }
+        public DateTimeOffset TaskCompletedTime { get; set; }
+        public DateTimeOffset TaskCreationDate { get; set; }
+        public string? QAResponsible { get; set; }
+        public DateTimeOffset QADoneTime { get; set; }
+        public string? Review { get; set; }
+        public string? ReviewRemarks { get; set; }
+        public string? TestCaseFunctional { get; set; }
+        public string? TestFeatureAndScenario { get; set; }
+        public string? WebRequestKey { get; set; }
+    }
+}
---
+++ src/BMS.UseCases/Tasks/Interfaces/ILoadTaskUseCase.cs
@@ -0,0 +1,9 @@
+﻿using BMS.CoreBusiness.Dtos;
+
+namespace BMS.UseCases.Tasks.Interfaces
+{
+    public interface ILoadTaskUseCase
+    {
+        Task<(IEnumerable<DevTaskDto>, int)> ExecuteAsync();
+    }
+}
