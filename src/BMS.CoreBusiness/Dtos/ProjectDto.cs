bcba1d0d95bee6a50e11247781012674b4690812
shovan <shovonmahmood67@gmail.com>
2023-03-07 11:42:27 +0600

    added project dto+viewModel

---
+++ src/BMS.CoreBusiness/Dtos/ProjectDto.cs
@@ -0,0 +1,16 @@
+﻿using BMS.CoreBusiness.Enums;
+
+namespace BMS.CoreBusiness.Dtos
+{
+    public sealed class ProjectDto
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public Guid CreatedById { get; init; }
+        public DateTimeOffset CreatedOnUtc { get; init; }
+        public Guid LastModifiedById { get; init; }
+        public DateTimeOffset? LastModifiedOnUtc { get; init; }
+        public string IPAddress { get; init; }
+        public Status Status { get; init; }
+    }
+}
---
+++ src/BMS.CoreBusiness/ViewModels/ProjectViewModel.cs
@@ -0,0 +1,18 @@
+﻿using BMS.CoreBusiness.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace BMS.CoreBusiness.ViewModels
+{
+    public sealed class ProjectViewModelCreate
+    {
+        [Required]
+        [StringLength(255, ErrorMessage = "Name is too long")]
+        public string? Name { get; set; }
+        public Guid CreatedById { get; init; }
+        public DateTimeOffset CreatedOnUtc { get; init; }
+        public Guid LastModifiedById { get; init; }
+        public DateTimeOffset? LastModifiedOnUtc { get; init; }
+        public string IPAddress { get; init; }
+        public Status Status { get; init; }
+    }
+}
